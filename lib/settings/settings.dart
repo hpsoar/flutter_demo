34d76e75a8237dfa6452711b560eb319d830e8ff
Huang Peng <huangpeng@chunyu.me>
2019-03-18 10:26:59 +0800

    MOD: add settings

---
+++ lib/settings/settings.dart
@@ -0,0 +1,45 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_utils/flutter_utils.dart';
+
+class SettingsPage extends StatefulWidget with UIHelper {
+  SettingsPage({Key key}) : super(key: key);
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  CYBridge native;
+
+  ListModel model = new ListModel();
+
+  void initialize() {
+    
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xfff1f0ee),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[ 
+            new Expanded(
+              child: model.build(controller: null),
+            ) 
+          ],
+      ),
+    );
+  }
+}
